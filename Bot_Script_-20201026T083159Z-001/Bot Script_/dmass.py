3c2b4544531a69744090f2663a931f5365d35520
puneeyon <72593306+puneeyon@users.noreply.github.com>
2020-10-26 14:17:19 +0530

    Update dmass.py

--- Bot_Script_-20201026T083159Z-001/Bot Script_/dmass.py
+++ Bot_Script_-20201026T083159Z-001/Bot Script_/dmass.py
@@ -44,4 +44,4 @@ async def send(ctx, *, content: str):
                 await client.say("DM can't Sent To : {} :x: ".format(member))
 
 
-client.run("YOUR BOT'S TOKEN HERE")                
+client.run("NzcwMTcxMjcwMDk1OTYyMTU0.X5Zr4w.eVycNe5lncqqf6ovr4CaqP_cI0o")                
